155f0829c173b202a8005b5d8369276714d99c14
Haoxiang Zhang <zhanghx04@gmail.com>
2024-08-07 18:42:39 +0000

    Implemented a hello world kernel module

---
+++ driver_dev/hello_kernel/hello.c
@@ -0,0 +1,20 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static int __init my_module_init(void) {
+	printk(KERN_INFO "Hello, Kernel!\n");
+	return 0;
+}
+
+static void __exit my_module_exit(void) {
+	printk(KERN_INFO "Goodbye, Kernel!\n")
+}
+
+
+module_init(my_module_init);
+module_exit(my_module_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Jonathan Zhang")
+MODULE_DESCRIPTION("A simple Hello World kernel module.");
